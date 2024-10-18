51a147731a7e3693d29317d5f3948dd2bd616cd6
Danni Garcia <jodadev.col@gmail.com>
2024-10-19 09:39:08 +1100

    Fix error in build proyect with the file fonts.ts and package.json

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -1,8 +1,7 @@
 import { Inter, Lusitana } from 'next/font/google'
 
-export const inter = Inter({ subsets: ['latin'], display: 'swap' })
+export const inter = Inter({ subsets: ['latin'] })
 export const lusitana = Lusitana({
   subsets: ['latin'],
-  display: 'swap',
   weight: ['400', '700']
 })
